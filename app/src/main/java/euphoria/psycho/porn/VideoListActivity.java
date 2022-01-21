181544b9abfc9ef3f27ff5ba8de4304fc82b2f42
grandiloquent <1785907865@qq.com>
2022-01-21 17:58:42 +0800

    改进播放器体验

--- app/src/main/java/euphoria/psycho/porn/VideoListActivity.java
+++ app/src/main/java/euphoria/psycho/porn/VideoListActivity.java
@@ -18,6 +18,8 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -33,6 +35,8 @@ public class VideoListActivity extends Activity {
     private GridView mGridView;
     private VideoItemAdapter mVideoItemAdapter;
     private String mDirectory;
+    private int mSort = 2;
+    public static final String KEY_SORT = "sort";
 
     private void addBookmark() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -67,27 +71,39 @@ public class VideoListActivity extends Activity {
 
     private void initialize() {
         mDirectory = SettingsFragment.getString(this, SettingsFragment.KEY_VIDEO_FOLDER, getDefaultPath());
-        loadFolder();
+        loadFolder(null, mSort);
     }
 
     private void loadDirectory() {
         SettingsFragment.setString(this, SettingsFragment.KEY_VIDEO_FOLDER, mDirectory);
-        loadFolder();
+        loadFolder(null, mSort);
     }
 
-    private void loadFolder() {
+    private void loadFolder(String filter, int sort) {
         File dir = new File(mDirectory);
-        File[] videos = dir.listFiles(pathname -> pathname.isFile() && pathname.getName().endsWith(".mp4"));
+        File[] videos = dir.listFiles(pathname -> pathname.isFile() && pathname.getName().endsWith(".mp4") && (filter == null || pathname.getName().contains(filter)));
         if (videos == null) {
             return;
         }
+        int direction = (sort & 1) == 0 ? -1 : 1;
         Arrays.sort(videos, (o1, o2) -> {
-            final long result = o2.lastModified() - o1.lastModified();
-            if (result < 0) {
-                return -1;
+            if ((sort & 2) == 2) {
+                final long result = o2.lastModified() - o1.lastModified();
+                if (result < 0) {
+                    return -1 * direction;
+                }
+                if (result > 0) {
+                    return 1 * direction;
+                }
             }
-            if (result > 0) {
-                return 1;
+            if ((sort & 4) == 4) {
+                final long result = o2.length() - o1.length();
+                if (result < 0) {
+                    return -1 * direction;
+                }
+                if (result > 0) {
+                    return 1 * direction;
+                }
             }
             return 0;
         });
@@ -121,6 +137,8 @@ public class VideoListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSort = PreferenceManager.getDefaultSharedPreferences(this)
+                .getInt(KEY_SORT, 3);
         setContentView(R.layout.video_list_activity);
         mGridView = findViewById(R.id.recycler_view);
         mGridView.setNumColumns(2);
@@ -138,7 +156,7 @@ public class VideoListActivity extends Activity {
     protected void onResume() {
         super.onResume();
         if (mDirectory != null)
-            loadFolder();
+            loadFolder(null, mSort);
     }
 
     @Override
@@ -152,7 +170,7 @@ public class VideoListActivity extends Activity {
             File dir = new File(mDirectory, item.getTitle().toString());
             File f = new File(videoItem.path);
             f.renameTo(new File(dir, f.getName()));
-            loadFolder();
+            loadFolder(null, mSort);
         }
         return super.onContextItemSelected(item);
     }
@@ -174,6 +192,20 @@ public class VideoListActivity extends Activity {
         getMenuInflater().inflate(R.menu.video_list, menu);
         menu.findItem(R.id.action_selector).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
         menu.findItem(R.id.action_bookmark).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                loadFolder(query, mSort);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -196,7 +228,51 @@ public class VideoListActivity extends Activity {
         } else if (item.getItemId() == R.id.action_create_directory) {
             createFileDirectory();
         }
+        if (item.getItemId() == R.id.action_sort_by_create_time_ascending) {
+            actionSortByCreateTimeAscending();
+        }
+        if (item.getItemId() == R.id.action_sort_by_create_time_descending) {
+            actionSortByCreateTimeDescending();
+        }
+        if (item.getItemId() == R.id.action_sort_by_size_ascending) {
+            actionSortBySizeAscending();
+        }
+        if (item.getItemId() == R.id.action_sort_by_size_descending) {
+            actionSortBySizeDescending();
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sort() {
+        PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .edit()
+                .putInt(KEY_SORT, mSort)
+                .apply();
+        loadFolder(null, mSort);
+    }
+
+    private void actionSortByCreateTimeAscending() {
+        mSort = 2;
+        sort();
+    }
+
+
+    private void actionSortByCreateTimeDescending() {
+        mSort = 3;
+        sort();
+    }
+
+
+    private void actionSortBySizeAscending() {
+        mSort = 4;
+        sort();
+    }
+
+
+    private void actionSortBySizeDescending() {
+        mSort = 5;
+        sort();
     }
 
 
